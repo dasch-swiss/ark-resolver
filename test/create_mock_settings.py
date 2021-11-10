c5a02c8118846ef6fa8b609b3bc02ec95522f052
irinaschubert <irina.schubert@dasch.swiss>
2021-11-10 12:39:13 +0100

    add make target to create test data

---
+++ test/create_mock_settings.py
@@ -0,0 +1,17 @@
+"""Creates a pickle file 'settings.pkl' with the configuration in ark-registry.ini. This file can then be used in the
+unit tests """
+
+import pickle as pkl
+import os
+import sys
+
+sys.path.append('src')
+
+from ark import load_settings
+
+if __name__ == "__main__":
+    config_path = "src/ark-config.ini"
+    os.environ['ARK_REGISTRY'] = 'src/ark-registry.ini'
+    settings = load_settings(config_path)
+    with open("test/settings.pkl", 'wb') as file:
+        pkl.dump(settings, file)
